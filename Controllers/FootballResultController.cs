1854bc6d4d9ef20f7b24a9dbf01d9ce321547c56
Nico0! <nicouzu96@hotmail.es>
2023-11-17 21:50:22 -0300

    Subida

---
+++ Controllers/FootballResultController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace service_process_football_result.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class FootballResultController : Controller
+    {
+         
+    }
+}
